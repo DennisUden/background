874056b6b1e13a704880fab71c0b3893213f410e
DennisUden <162509535+DennisUden@users.noreply.github.com>
2025-03-17 12:04:40 +0100

    Create background.bat

---
+++ background.bat
@@ -0,0 +1,6 @@
+@echo off
+echo hello 
+
+mkdir temp 2>nul
+transparent-background --source ${PWD} --format png --mode base-nightly --resize static --threshold 0.1 --dest temp
+for %%f in (temp\*.png) do (magick "%%f" -trim -fuzz 10 "%%f")
