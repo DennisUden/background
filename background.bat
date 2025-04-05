505bad804a3062534b88e3f7b67b6e40cb2552d9
Dennis Uden <162509535+DennisUden@users.noreply.github.com>
2025-04-05 15:30:51 +0200

    Update background.bat

--- background.bat
+++ background.bat
@@ -5,7 +5,7 @@ mkdir "converted-to-png" 2>nul
 mkdir "background-removed" 2>nul
 
 for %%f in (*.*) do (
-  magick "%%f" -convert "converted-to-png\%%f.png"
+  magick "%%f" "converted-to-png\%%f.png"
 )
 
 transparent-background --source "%cd%\converted-to-png" --format png --mode base-nightly --resize static --threshold 0.1 --dest "%cd%\background-removed"
