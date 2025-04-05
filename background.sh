edd6746c2ba6fd968151451dbfebdba74c1a8000
Dennis Uden <162509535+DennisUden@users.noreply.github.com>
2025-04-05 15:55:07 +0200

    Update background.sh

--- background.sh
+++ background.sh
@@ -6,11 +6,11 @@ mkdir -p "converted-to-png"
 mkdir -p "background-removed"
 
 for f in *.* ; do
-  magick "$f" "converted-to-png/${f%.*}.png"
+  convert "$f" "converted-to-png/${f%.*}.png"
 done
 
 transparent-background --source "${PWD}/converted-to-png" --format png --mode base-nightly --resize static --threshold 0.1 --dest "${PWD}/background-removed"
 
 for f in ./background-removed/*.png ; do 
-  magick "$f" -trim "$f"
+  convert "$f" -trim "$f"
 done
